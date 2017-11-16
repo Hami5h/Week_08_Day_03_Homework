49aa1a26456fdcb31bfe28a44e234d6f5fd00d2b
Hamish <hamishfpso@gmail.com>
2017-11-16 08:32:41 +0000

    back button added to results page

--- app/src/main/java/com/example/hamishstewart/rockpaperscissors/Controllers/ResultsActivity.java
+++ app/src/main/java/com/example/hamishstewart/rockpaperscissors/Controllers/ResultsActivity.java
@@ -3,12 +3,16 @@ package com.example.hamishstewart.rockpaperscissors.Controllers;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
 
+import com.example.hamishstewart.rockpaperscissors.Models.Game;
 import com.example.hamishstewart.rockpaperscissors.R;
 
 public class ResultsActivity extends AppCompatActivity {
     TextView resultView;
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,7 @@ public class ResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_results);
 
         resultView = findViewById(R.id.resultView);
+        backButton = findViewById(R.id.backButton);
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -23,6 +28,12 @@ public class ResultsActivity extends AppCompatActivity {
 
         TextView answerTextView = findViewById(R.id.resultView);
         answerTextView.setText(gameResult);
+
+    }
+
+    public void onBackButtonClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
 
     }
 
